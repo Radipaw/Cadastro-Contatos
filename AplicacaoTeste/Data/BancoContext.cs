dc514e6ea04ab00b3d97a7c5d36cde6da69974a4
quart <quart@IDEAPADQUARTER>
2025-03-31 16:49:41 -0300

    Fazendo alterações no banco de dados

--- AplicacaoTeste/Data/BancoContext.cs
+++ AplicacaoTeste/Data/BancoContext.cs
@@ -1,6 +1,14 @@
-﻿namespace AplicacaoTeste.Data
+﻿using AplicacaoTeste.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AplicacaoTeste.Data
 {
-    public class BancoContext
+    public class BancoContext : DbContext
     {
+        public BancoContext(DbContextOptions<BancoContext> options) :base(options) { 
+            
+
+        }
+        public DbSet<ContatosModel> Contatos { get; set;}
     }
 }
